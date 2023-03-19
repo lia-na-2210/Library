5816456a11a0bcaa452b24837a0e72d49bdb6fcd
lia-na-2210 <naneko.bl@gmail.com>
2023-03-18 18:00:22 -0600

    form toggle and data storage

--- main.js
+++ main.js
@@ -1,9 +1,53 @@
-const input = document.getElementById('title-inp');
+const addButton = document.getElementById('add-book');
+const form = document.querySelector('form');
+const myLibrary = [
+  {
+    title: 'Artemis Fowl',
+    author: 'Eoin Colfer',
+    pages: '288',
+    checl: 'Yes',
+  },
+  {
+    title: '1984',
+    author: 'George Orwell',
+    pages: '206',
+    checl: 'Yes',
+  },
+  {
+    title: 'Servant of the Bones',
+    author: 'Anne Rice',
+    pages: '387',
+    checl: 'No',
+  },
+  {
+    title: 'Harry Potter and the Prisioner of Azkaban',
+    author: 'J. K. Rowling',
+    pages: '435',
+    checl: 'No',
+  },
+];
 
-input.addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') {
-    event.preventDefault();
-    const text = document.createElement('input');
-    document.body.appendChild(text);
+addButton.addEventListener('click', () => {
+  const formArea = document.getElementById('form-area');
+  if (formArea.style.display === 'none' || formArea.style.display === null) {
+    formArea.style.display = 'block';
+  } else {
+    formArea.style.display = 'none';
   }
+});
+
+/* function AddBook(title, author, pages, check) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.check = check;
+} */
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const fd = new FormData(form);
+  const book = Object.fromEntries(fd);
+  myLibrary.push(book);
+  form.reset();
+  console.log(myLibrary);
 });
