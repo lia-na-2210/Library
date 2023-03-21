91a9d672a103942a4886bf758276caa610bc4e4d
lia-na-2210 <naneko.bl@gmail.com>
2023-03-20 20:20:52 -0600

    dynamic grid, read check

--- main.js
+++ main.js
@@ -5,25 +5,25 @@ const myLibrary = [
     title: 'Artemis Fowl',
     author: 'Eoin Colfer',
     pages: '288',
-    checl: 'Yes',
+    check: 'Yes',
   },
   {
     title: '1984',
     author: 'George Orwell',
     pages: '206',
-    checl: 'Yes',
+    check: 'Yes',
   },
   {
     title: 'Servant of the Bones',
     author: 'Anne Rice',
     pages: '387',
-    checl: 'No',
+    check: 'No',
   },
   {
     title: 'Harry Potter and the Prisioner of Azkaban',
     author: 'J. K. Rowling',
     pages: '435',
-    checl: 'No',
+    check: 'No',
   },
 ];
 
@@ -36,18 +36,66 @@ addButton.addEventListener('click', () => {
   }
 });
 
-/* function AddBook(title, author, pages, check) {
+function Addelement(title, author, pages, check) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.check = check;
-} */
+}
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const fd = new FormData(form);
-  const book = Object.fromEntries(fd);
-  myLibrary.push(book);
+  const element = Object.fromEntries(fd);
+  myLibrary.push(element);
+  newCard(element);
   form.reset();
   console.log(myLibrary);
 });
+
+function newCard(element) {
+  const newTitle = element.title;
+  const newAuthor = element.author;
+  const newPages = element.pages;
+  const newCheck = element.check;
+  const grid = document.getElementById('grid');
+  const div = document.createElement('div');
+  div.className = 'card';
+  div.innerHTML = `
+  <h3>${newTitle}</h3>
+  <h4>${newAuthor}</h4>
+  <p>${newPages} pages</p>
+  `;
+  grid.appendChild(div);
+}
+
+function createCard() {
+  const grid = document.getElementById('grid');
+  myLibrary.forEach((element) => {
+    const newTitle = element.title;
+    const newAuthor = element.author;
+    const newPages = element.pages;
+    const newCheck = element.check;
+    console.log(newCheck);
+
+    const check = readCheck(newCheck);
+    const div = document.createElement('div');
+    div.className = 'card';
+    div.innerHTML = `
+    <h3>${newTitle}</h3>
+    <h4>${newAuthor}</h4>
+    <p>${newPages} pages</p>
+    <p>${check}</p>
+    `;
+    grid.appendChild(div);
+  });
+}
+
+function readCheck(choice) {
+  if (choice === 'Yes') {
+    return ('I have read this book ✔️');
+  }
+  return ("I haven't read this yet ❌");
+}
+
+window.onload = createCard();
